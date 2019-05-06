f22f8f440c1b0c9b07cf0e5466b6fbf0affae9cc
shidono <chedli.elloumi@esprit.tn>
2019-05-06 09:42:41 +0100

    effacer tout

--- enigmefiles/structures.h
+++
@@ -1,6 +0,0 @@
-
-void initialiserenigmehack(simple map,SDL_Surface *3pos1,SDL_Surface *3pos2,SDL_Surface *3pos3,SDL_Surface *3pos4,SDL_Surface *2pos1,SDL_Surface *2pos2,simple locked,simple unlocked,protocol firstleft,protocol middle,protocol bottom,protocol final2pos,protocol final3pos);
-void afficherenigmehack(SDL_Surface *screen,simple map,SDL_Surface *3pos1,SDL_Surface *3pos2,SDL_Surface *3pos3,SDL_Surface *3pos4,SDL_Surface *2pos1,SDL_Surface *2pos2,simple locked,simple unlocked,protocol firstleft,protocol middle,protocol bottom,protocol final2pos,protocol final3pos);
-
-void initialiserenigmeserrure(serrure lockpick,serrure *lockfield[3],serrure unlocked);
-void afficherenigmeserrure(serrure *lockpick,serrure *lockfield[3],serrue *unlocked);
